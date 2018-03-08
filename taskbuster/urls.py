b7798412f78a7534c8ca3b6000de3d792aa74a2b
rolkov <rolkov+git@gmail.com>
2018-03-08 21:05:19 +0100

    Added website files and testing with coverage.

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .views import home
+from .views import home_files
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    re_path(r'^(?P<filename>(robots.txt)|(humans.txt))$',
+            home_files, name='home-files'),
 ]
